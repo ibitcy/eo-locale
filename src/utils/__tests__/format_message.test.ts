decfb2da4b82683c3d4af8697b34e8d19f794c7a
Maksim Sharipov <msharipov@expertoption.com>
2018-07-07 11:36:33 +0300

    [Fix] Fix tests

--- src/utils/__tests__/format_message.test.ts
+++ src/utils/__tests__/format_message.test.ts
@@ -1,39 +1,42 @@
 import { convertObjectToMap } from 'eo-utils';
 
 import { createMessageFormatter } from '..';
+import { serverPolyfill } from '../../polyfill';
+
+serverPolyfill(['en', 'ru']);
 
 const locales = {
-	en: {
-		a: 'Hello {name}!'
-	},
-	ru: {
-		a: 'Привет {name}!'
-	}
+  en: {
+    a: 'Hello {name}!',
+  },
+  ru: {
+    a: 'Привет {name}!',
+  },
 };
 
 const formatMessageEn = createMessageFormatter(
-	'en',
-	convertObjectToMap(locales.en)
+  'en',
+  convertObjectToMap(locales.en),
 );
 const formatMessageRu = createMessageFormatter(
-	'ru',
-	convertObjectToMap(locales.ru)
+  'ru',
+  convertObjectToMap(locales.ru),
 );
 
 describe('formatMessage', () => {
-	it('Should return "Hello World!"', () => {
-		expect(
-			formatMessageEn('a', {
-				name: 'World'
-			})
-		).toBe('Hello World!');
-	});
+  it('Should return "Hello World!"', () => {
+    expect(
+      formatMessageEn('a', {
+        name: 'World',
+      }),
+    ).toBe('Hello World!');
+  });
 
-	it('Should return "Привет Мир!"', () => {
-		expect(
-			formatMessageRu('a', {
-				name: 'Мир'
-			})
-		).toBe('Привет Мир!');
-	});
+  it('Should return "Привет Мир!"', () => {
+    expect(
+      formatMessageRu('a', {
+        name: 'Мир',
+      }),
+    ).toBe('Привет Мир!');
+  });
 });
