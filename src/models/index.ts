b26a45bf42de5f91b598e800323fe1b6835742be
Maksim Sharipov <msharipov@expertoption.com>
2018-11-20 11:52:08 +0200

    Update tests

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,12 +1,8 @@
 export type TMessage = string | number;
 
-export interface IMessages {
-	[index: string]: TMessage;
-}
-
 export interface ILocale {
 	language: string;
-	messages: IMessages;
+	messages: Record<string, TMessage>;
 }
 
 export type Omit<T, K> = Pick<T, Exclude<keyof T, K>>;
