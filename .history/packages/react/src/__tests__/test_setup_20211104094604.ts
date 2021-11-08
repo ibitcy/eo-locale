ce532572372cb9c3cd87dbd83c26eb3a635247a5
Maksim Sharipov <m.v.sharipov@gmail.com>
2021-11-09 00:58:16 +0100

    chore: remove .history folder

--- .history/packages/react/src/__tests__/test_setup_20211104094604.ts
+++
@@ -1,7 +0,0 @@
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-global.Intl = require('intl');
-require('intl-pluralrules');
-
-Enzyme.configure({ adapter: new Adapter() });
--- .history/packages/react/src/__tests__/test_setup_20211104102015.ts
+++
@@ -1,7 +0,0 @@
-import Enzyme from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
-
-global.Intl = require('intl');
-require('intl-pluralrules');
-
-Enzyme.configure({ adapter: new Adapter() });
